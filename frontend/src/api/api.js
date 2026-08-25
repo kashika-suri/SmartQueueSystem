b30eaa33c786de59029f6997601b6eccdb563994
kashika-suri <kashika.suri@outlook.com>
2026-08-25 04:33:02 +0100

    Update API URL to Render

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smartqueuesystem-production.up.railway.app/api",
+  baseURL: "https://smartqueuesystem-219o.onrender.com/api",
 });
 
 API.interceptors.request.use(
